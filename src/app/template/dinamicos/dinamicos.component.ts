d2cd79225db967f83d342e41996043ad31023364
richard <richard_allcca_llano@hotmail.com>
2022-10-24 18:57:05 -0500

    Template - Formualios dinamicos

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,16 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface Persona {
+   nombre: string;
+   favoritos: Favorito[];
+}
+interface Favorito {
+   id: number;
+   nombre: string;
+}
 
 @Component({
-  selector: 'app-dinamicos',
-  templateUrl: './dinamicos.component.html',
-  styles: [
-  ]
+   selector: 'app-dinamicos',
+   templateUrl: './dinamicos.component.html',
+   styles: [
+   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+   @ViewChild('dinamicForm') dinamicForm!: NgForm
 
-  ngOnInit(): void {
-  }
+   persona: Persona = {
+      nombre: "Richard",
+      favoritos: [
+         { id: 1, nombre: "Metal Gear" },
+         { id: 2, nombre: "Yu Gi Oh!" }
+      ]
+   }
+
+   constructor() { }
+
+
+   nombreValido(): boolean {
+      return this.dinamicForm?.controls['nombre']?.invalid
+         && this.dinamicForm?.controls['nombre']?.touched;
+   }
+
+   guardar() {
+
+   }
 
 }
